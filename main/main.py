562f5a4424a1ea609a3669156ac01fe5006b236d
pizza <pizza343@live.com>
2018-10-06 19:04:01 -0400

    updated board construction with checkers objects

--- main/main.py
+++ main/main.py
@@ -5,12 +5,15 @@ class Checker:
     x = 0
     y = 0
 
-    def __init__(self, team, x, y):
+    def __init__(self, team, y, x):
         self.team = team
+
+    def toString(self):
+        return self.team
 
 if __name__ == '__main__':
     # TODO fully implement this flag
-    player = "Black"
+    player = "B"
     # initialize the board as a double array with none values in each space
     board = [None] * 8
     for i in range(8):
@@ -18,25 +21,25 @@ if __name__ == '__main__':
 
     # add the checkers to each space properly for pregame setup
     for i in range(8):
-        # lower part of board being initialized
+        # lower part of board being initialized (Black)
         if i in range(0,3):
             for j in range(8):
                 # if the space coordinates added together are divisible by 2
                 # a checker should be placed on that coordinate
                 if (i + j) % 2 == 0:
                     # TODO add a checker here properly instead of an empty string
-                    board[i][j] = 'B'
+                    board[i][j] = Checker("B",i,j)
                 # make the space empty
                 else:
                     board[i][j] = '0'
-        # upper part of board being initialized
+        # upper part of board being initialized (White)
         elif i in range(5,8):
             for j in range(8):
                 # if the space coordinates added together are divisible by 2
                 # a checker should be placed on that coordinate
                 if (i + j) % 2 == 0:
                     # TODO add a checker here properly instead of an empty string
-                    board[i][j] = 'W'
+                    board[i][j] = Checker("W",i,j)
                 # make the space empty
                 else:
                     board[i][j] = '0'
@@ -51,7 +54,7 @@ if __name__ == '__main__':
 
     # print out the board setup for testing. Comment this out if the above code is fully tested
     # black player orientation
-    if player == "Black":
+    if player == "B":
         temp = []
         for i in range(0,4):
             # here I want to reverse the first list of the board (rows)
@@ -69,9 +72,12 @@ if __name__ == '__main__':
                 board[i][j] = board[i][7 - j]
                 board[i][7 - j] = temp
 
-
     for i in range(0,8):
         line = ' '
         for j in range(0,8):
-            line += board[i][j]
+            if isinstance(board[i][j],Checker):
+                line += board[i][j].toString()
+            else:
+                line += board[i][j]
         print(line)
+
